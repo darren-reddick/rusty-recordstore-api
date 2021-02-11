083cdf1f3fd639f61165268c1336a379a4df5b18
dreddick.home2@gmail.com <dreddick.home2@gmail.com>
2021-02-11 19:49:18 +0000

    fix clippy recommendations

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -3,40 +3,40 @@ use std::convert::Infallible;
 use crate::models;
 
 pub async fn get_items(db: models::SafeDB) -> Result<impl warp::Reply, Infallible> {
-  let c = db.lock().await;
-  let c = &*c;
-  Ok(warp::reply::json(&c.get_items()))
+    let c = db.lock().await;
+    let c = &*c;
+    Ok(warp::reply::json(&c.get_items()))
 }
 
 pub async fn get_item(uuid: String, db: models::SafeDB) -> Result<impl warp::Reply, Infallible> {
-  let c = db.lock().await;
-  let c = &*c;
-  Ok(warp::reply::json(&c.get_item(uuid)))
+    let c = db.lock().await;
+    let c = &*c;
+    Ok(warp::reply::json(&c.get_item(uuid)))
 }
 
 pub async fn delete_item(uuid: String, db: models::SafeDB) -> Result<impl warp::Reply, Infallible> {
-  let c = &mut db.lock().await;
-  let c = &mut *c;
-  c.delete_item(uuid).unwrap();
-  Ok(warp::reply::json(&{}))
+    let c = &mut db.lock().await;
+    let c = &mut *c;
+    c.delete_item(uuid).unwrap();
+    Ok(warp::reply::json(&{}))
 }
 
 pub async fn add_item(
-  db: models::SafeDB,
-  item: models::Item,
+    db: models::SafeDB,
+    item: models::Item,
 ) -> Result<impl warp::Reply, Infallible> {
-  let c = &mut db.lock().await;
-  let c = &mut *c;
-  Ok(warp::reply::json(&c.add_item(item)))
+    let c = &mut db.lock().await;
+    let c = &mut *c;
+    Ok(warp::reply::json(&c.add_item(item)))
 }
 
 pub async fn update_item(
-  uuid: String,
-  db: models::SafeDB,
-  item: models::Item,
+    uuid: String,
+    db: models::SafeDB,
+    item: models::Item,
 ) -> Result<impl warp::Reply, Infallible> {
-  let c = &mut db.lock().await;
-  let c = &mut *c;
-  c.update_item(uuid, item).unwrap();
-  Ok(warp::reply::json(&{}))
+    let c = &mut db.lock().await;
+    let c = &mut *c;
+    c.update_item(uuid, item).unwrap();
+    Ok(warp::reply::json(&{}))
 }
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -8,100 +8,100 @@ use uuid::Uuid;
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Item {
-  pub uuid: Option<String>,
-  pub title: String,
-  pub artist: String,
-  pub format: String,
-  pub year: u16,
+    pub uuid: Option<String>,
+    pub title: String,
+    pub artist: String,
+    pub format: String,
+    pub year: u16,
 }
 
 impl Item {
-  pub fn add_uuid(&mut self) -> Result<String, &str> {
-    match &self.uuid {
-      None => {
-        let _uuid = Uuid::new_v4().to_string();
-        self.uuid = Some(_uuid.clone());
-        Ok(_uuid)
-      }
-      Some(_) => Err("UUID is already set"),
+    pub fn add_uuid(&mut self) -> Result<String, &str> {
+        match &self.uuid {
+            None => {
+                let _uuid = Uuid::new_v4().to_string();
+                self.uuid = Some(_uuid.clone());
+                Ok(_uuid)
+            }
+            Some(_) => Err("UUID is already set"),
+        }
     }
-  }
-  pub fn new(title: String, artist: String, format: String, year: u16) -> Item {
-    Item {
-      uuid: None,
-      title,
-      artist,
-      format,
-      year,
+    pub fn new(title: String, artist: String, format: String, year: u16) -> Item {
+        Item {
+            uuid: None,
+            title,
+            artist,
+            format,
+            year,
+        }
     }
-  }
 }
 
 pub type SafeDB = Arc<Mutex<Box<dyn DB + Send>>>;
 
 pub trait DB {
-  fn get_items(&self) -> Vec<Item>;
-  fn add_item(&mut self, item: Item) -> Item;
-  fn get_item(&self, uuid: String) -> &Item;
-  fn delete_item(&mut self, uuid: String) -> Result<(), String>;
-  fn update_item(&mut self, uuid: String, item: Item) -> Result<(), String>;
+    fn get_items(&self) -> Vec<Item>;
+    fn add_item(&mut self, item: Item) -> Item;
+    fn get_item(&self, uuid: String) -> &Item;
+    fn delete_item(&mut self, uuid: String) -> Result<(), String>;
+    fn update_item(&mut self, uuid: String, item: Item) -> Result<(), String>;
 }
 
 #[cfg(test)]
 mod tests {
 
-  use super::*;
-  use std::collections::HashMap;
-  #[test]
-  fn empty_database() {
-    let db = inmemdb::Database {
-      item_table: HashMap::new(),
-    };
+    use super::*;
+    use std::collections::HashMap;
+    #[test]
+    fn empty_database() {
+        let db = inmemdb::Database {
+            item_table: HashMap::new(),
+        };
 
-    let items = db.get_items();
+        let items = db.get_items();
 
-    assert_eq!(items.len(), 0)
-  }
+        assert_eq!(items.len(), 0)
+    }
 
-  #[test]
-  fn add_and_get_person() {
-    let mut db = inmemdb::Database {
-      item_table: HashMap::new(),
-    };
+    #[test]
+    fn add_and_get_person() {
+        let mut db = inmemdb::Database {
+            item_table: HashMap::new(),
+        };
 
-    let item = db.add_item(Item {
-      uuid: None,
-      title: "Papua New Guinea".to_string(),
-      artist: "Future Sound of London".to_string(),
-      format: "vinyl".to_string(),
-      year: 1991,
-    });
+        let item = db.add_item(Item {
+            uuid: None,
+            title: "Papua New Guinea".to_string(),
+            artist: "Future Sound of London".to_string(),
+            format: "vinyl".to_string(),
+            year: 1991,
+        });
 
-    assert_eq!(item.year, 1991);
+        assert_eq!(item.year, 1991);
 
-    let get = db.get_item(item.uuid.unwrap());
+        let get = db.get_item(item.uuid.unwrap());
 
-    assert_eq!(get.format, "vinyl");
-  }
+        assert_eq!(get.format, "vinyl");
+    }
 
-  #[test]
-  fn add_and_delete_person() {
-    let mut db = inmemdb::Database {
-      item_table: HashMap::new(),
-    };
+    #[test]
+    fn add_and_delete_person() {
+        let mut db = inmemdb::Database {
+            item_table: HashMap::new(),
+        };
 
-    let item = db.add_item(Item {
-      uuid: None,
-      title: "Dark and Long".to_string(),
-      artist: "Underworld".to_string(),
-      format: "tape".to_string(),
-      year: 1993,
-    });
+        let item = db.add_item(Item {
+            uuid: None,
+            title: "Dark and Long".to_string(),
+            artist: "Underworld".to_string(),
+            format: "tape".to_string(),
+            year: 1993,
+        });
 
-    assert_eq!(item.year, 1993);
+        assert_eq!(item.year, 1993);
 
-    let _ = db.delete_item(item.uuid.unwrap());
+        let _ = db.delete_item(item.uuid.unwrap());
 
-    assert_eq!(db.item_table.len(), 0);
-  }
+        assert_eq!(db.item_table.len(), 0);
+    }
 }
